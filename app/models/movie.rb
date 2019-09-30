33ebbf89e6b139bdf0d5b7277b135b4d29ba67a7
EC2 Default User <ec2-user@ip-172-31-10-134.us-east-2.compute.internal>
2019-09-30 00:33:23 +0000

    Sort Correction

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        %w(G PG PG-13 NC-17 R)
-    end
-    
-    def self.with_ratings(ratings)
-        where(rating: ratings)
+        self.all.select(:rating).distinct.pluck(:rating)
     end
 end
